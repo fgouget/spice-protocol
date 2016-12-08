8ce16fc4263025be224a30f2936758b3dd793052
Francois Gouget <fgouget@codeweavers.com>
2016-12-08 05:38:15 +0100

    macros: A few endianness spelling fixes
    Signed-off-by: Francois Gouget <fgouget@codeweavers.com>

--- spice/macros.h
+++ spice/macros.h
@@ -385,7 +385,7 @@
 #endif /* generic */
 
 
-/* detect endianess */
+/* detect endianness */
 #undef SPICE_ENDIAN
 #define SPICE_ENDIAN_LITTLE 4321
 #define SPICE_ENDIAN_BIG    1234
@@ -433,11 +433,11 @@
 #endif
 
 #if !defined(SPICE_ENDIAN)
-#error Unable to detect processor endianess
+#error Unable to detect processor endianness
 #endif
 
 #if SPICE_ENDIAN == SPICE_ENDIAN_PDP
-#error PDP endianess not supported by Spice
+#error PDP endianness not supported by Spice
 #endif
 
 
